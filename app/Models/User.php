b6f69622afad0257c23b37363176c0feac202416
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-05-29 14:01:29 +0100

    Admin user getter

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +44,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function isAdmin(): Attribute
+    {
+        $admins = ['fife@me.com'];
+        return Attribute::make(
+            get: fn() => in_array($this->email, $admins)
+        );
+    }
 }
